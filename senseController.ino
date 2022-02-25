7f68cfbb7df98498feaf77c89065ededaecbb6b5
Jared Jackson <jared.b.j@gmail.com>
2022-02-24 23:14:48 -0500

    Working websocket connection.
    Moved authenticate into loop. Incorporated JSON parsing of auth request and websocket data.
    WIP parsing the large realtime websocket data.

--- senseController.ino
+++ senseController.ino
@@ -1,6 +1,7 @@
 #include <SPI.h>
 #include <WiFiNINA.h>
 #include <ArduinoHttpClient.h>
+#include <Arduino_JSON.h>
 #include "arduino_secrets.h"
 
 ///////please enter your sensitive data in the Secret tab/arduino_secrets.h
@@ -9,14 +10,22 @@ char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as k
 char senseUser[] = SECRET_SENSE_ID;
 char sensePass[] = SECRET_SENSE_PASS;
 
-char server[] = "api.sense.com";
+const char server[] = "api.sense.com";
 const char AUTH_URL[] = "/apiservice/api/v1/authenticate";
+const char socket[] = "clientrt.sense.com";
+String socketURL = "/monitors/";
+
 //char server[] = "httpbin.org";
 //const char AUTH_URL[] = "/post";
 int port = 443;
 WiFiSSLClient wifi;
 HttpClient client= HttpClient(wifi, server, port);
+WebSocketClient wsClient = WebSocketClient(wifi, socket, port);
 int status = WL_IDLE_STATUS;
+
+String SENSE_ACCESS_TOKEN = "none";
+String SENSE_MONITOR_ID = "none";
+
 
 //set interval for sending messages (milliseconds)
 const long interval = 8000;
@@ -33,6 +42,7 @@ void setup() {
 
   // attempt to connect to Wifi network:
   Serial.println("Startuping");
+  
   while (status != WL_CONNECTED) {
 
     Serial.print("Attempting to connect to SSID: ");
@@ -51,6 +61,69 @@ void setup() {
 
   Serial.println("Connected to wifi");
   printWiFiStatus();
+
+}
+
+void loop() {
+  //if token - get data
+  if (SENSE_ACCESS_TOKEN != "none") {
+    Serial.println("starting WebSocket client");
+    wsClient.begin(socketURL);
+
+    while (wsClient.connected()) {
+      int messageSize = wsClient.parseMessage();
+      String message = "";
+      if (messageSize > 0) {
+          Serial.print("Received a message:");
+          message = wsClient.readString();
+          Serial.println(message);
+          JSONVar messageData = JSON.parse(message);
+          Serial.println(messageData);
+          String mType = JSON.stringify(messageData["type"]);
+          Serial.print("mType: ");
+          Serial.println(mType);
+//          ?????
+          if (!mType){
+            Serial.println("try to recover string");
+            int index = message.indexOf("d_w");
+            String subMessage = message.substring(index);
+            String newMessage = "{\"payload\":{";
+            newMessage += subMessage;
+            messageData = JSON.parse(newMessage);
+            Serial.print("newMessage: ");
+            Serial.println(messageData);
+            mType = "realtime_update";
+          }
+          if (mType == "realtime_update") {
+            Serial.print("Received a realtime_update: ");
+            Serial.println(messageData);
+            int solar = messageData["payload"]["d_solar_w"];
+            int grid = messageData["payload"]["d_w"];
+            int avail = solar - grid;
+            Serial.print("solar: ");
+            Serial.println(solar);
+            Serial.print("grid: ");
+            Serial.println(grid);
+            Serial.print("avail: ");
+            Serial.println(avail);
+            Serial.println("Wait 1s for next message");
+            delay(1000);
+          }
+       }
+      
+       // wait 5 seconds
+      //parse data
+      //get difference
+      
+     }
+  }
+  else {
+    authenticate();
+  }
+  
+}
+
+void authenticate() {
   Serial.println("\nStarting connection to server...");
 
   Serial.println("making POST request");
@@ -70,42 +143,28 @@ void setup() {
   Serial.println(statusCode);
   Serial.print("Response: ");
   Serial.println(response);
-  Serial.println("Startup done.");
-
-  //todo - do JSON parsing next
-
-  //if status 200, then save access_token
+  
+  if (statusCode == 200) {
+    JSONVar htmlResponse = JSON.parse(response);
+    SENSE_ACCESS_TOKEN = htmlResponse["access_token"];
+    SENSE_MONITOR_ID = JSON.stringify(htmlResponse["monitors"][0]["id"]);
+    constructWebSocketURL();
+  }
+  else {
+    Serial.print("Authentication Failed: ");
+    Serial.println(statusCode);
+  }
 }
 
-void loop() {
-//  Serial.println("loop");
-  // if there are incoming bytes available
-
-  // from the server, read them and print them:
-
-//  while (client.available()) {
-//
-//    char c = client.read();
-//
-//    Serial.write(c);
-//
-//  }
-
-  // if the server's disconnected, stop the client:
-
-//  if (!client.connected()) {
-//
-//    Serial.println();
-//
-//    Serial.println("disconnecting from server.");
-//
-//    client.stop();
-//
-//    // do nothing forevermore:
-//
-//    while (true);
-//
-//  }
+void constructWebSocketURL() {
+  //wss://clientrt.sense.com/monitors/213413/realtimefeed?access_token=t1.v2.eyJhbGciOiJIUzUxMiJ9.&sense_device_id=ldv..55&sense_protocol=8&sense_client_type=web
+  socketURL += SENSE_MONITOR_ID;
+  socketURL += "/realtimefeed?access_token=";
+  socketURL += SENSE_ACCESS_TOKEN;
+//  socketURL += "&sense_device_id=ldvhhsf2ehhfagv49g8r8vuunt7xgq2zafo7egjojhv9ljre5acvjkuvd7ugyemezcvdmg795n5qkipaungw2i38ptjmjjxz8se6hbiusmyi122baq1ow3g7z799f055&sense_protocol=8&sense_client_type=web";
+//socketURL += "&sense_protocol=8&sense_client_type=web";
+  Serial.print("WebSocketURL: ");
+  Serial.println(socketURL);
 }
 
 void printWiFiStatus() {
