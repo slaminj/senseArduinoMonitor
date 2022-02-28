5747cb2dfda11da31a4b31a48445c60e74c22b71
Jared Jackson <theslaminj@gmail.com>
2022-02-27 22:55:39 -0500

    Pass callback to senseable.

--- pi/senseController.py
+++ pi/senseController.py
@@ -3,10 +3,25 @@ from sense_energy  import Senseable
 
 sense = Senseable()
 sense.authenticate(SENSE_USER, SENSE_PASS)
-sense.update_realtime()
-# sense.update_trend_data()
-print ("Active:", sense.active_power, "W")
-print ("Active Solar:", sense.active_solar_power, "W")
-print ("Daily:", sense.daily_usage, "KWh")
-print ("Daily Solar:", sense.daily_production, "KWh")
-# print ("Active Devices:",", ".join(sense.active_devices)) 
+
+class senseController:
+
+    pastPower= 0
+    pastSolar= 0
+
+    def webSocketCallback(self):
+        # sense.update_trend_data()
+        currPower = int(sense.active_power)
+        if currPower != self.pastPower:
+            print ("Active:", currPower, "W")
+            self.pastPower = currPower
+
+        currSolar = int(sense.active_solar_power)
+        if currSolar != self.pastSolar:
+            print ("Active Solar:", currSolar, "W")
+            self.pastSolar = currSolar
+        # print ("Daily:", sense.daily_usage, "KWh")
+        # print ("Daily Solar:", sense.daily_production, "KWh")
+        # print ("Active Devices:",", ".join(sense.active_devices)) 
+sC = senseController()
+sense.update_realtime(sC.webSocketCallback)
