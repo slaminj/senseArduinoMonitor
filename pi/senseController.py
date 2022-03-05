6261a94b6c9334a30d01b5bac5ea191990e5f6db
Jared Jackson <theslaminj@gmail.com>
2022-03-05 09:31:43 -0500

    Added logging, error restart, charge/discharge logic.

--- pi/senseController.py
+++ pi/senseController.py
@@ -1,5 +1,8 @@
 from secrets import SENSE_PASS, SENSE_USER
 from sense_energy  import Senseable
+from time import time, gmtime, strftime
+import logging
+logging.basicConfig(filename='output.log', encoding='utf-8', level=logging.DEBUG, format='%(asctime)s %(message)s')
 
 sense = Senseable()
 sense.authenticate(SENSE_USER, SENSE_PASS)
@@ -8,20 +11,42 @@ class senseController:
 
     pastPower= 0
     pastSolar= 0
+    updateInterval = 3
+    lastCall = 0
+    
+    chargeW = 400
+    charging = False
+    
+    def __init__(self):
+        self.lastCall = time()
 
-    def webSocketCallback(self):
-        # sense.update_trend_data()
+    def webSocketCallback(self, error):
+        print("heartbeat. error: ", error)
+        if (error):
+            sense.update_realtime(sC.webSocketCallback)
+            #return
         currPower = int(sense.active_power)
-        if currPower != self.pastPower:
-            print ("Active:", currPower, "W")
-            self.pastPower = currPower
-
         currSolar = int(sense.active_solar_power)
-        if currSolar != self.pastSolar:
-            print ("Active Solar:", currSolar, "W")
-            self.pastSolar = currSolar
-        # print ("Daily:", sense.daily_usage, "KWh")
-        # print ("Daily Solar:", sense.daily_production, "KWh")
-        # print ("Active Devices:",", ".join(sense.active_devices)) 
+        print(self.lastCall, self.updateInterval, time())
+        if (time() > self.lastCall + self.updateInterval):
+            logging.info("ActivePower: %sW, ActiveSolar: %sW", currPower, currSolar)
+            difference = currSolar - currPower
+            self.lastCall = time()
+            if (self.charging and difference > 0):
+                logging.info("charging")
+            elif (difference > (self.chargeW + (self.chargeW * 0.10))):
+                self.startCharging()
+            else:
+                self.startDischarging();
+                
+    def startCharging(self):
+        logging.info("start charging")
+        #enable pins for relay
+        
+    def startDischarging(self):
+        logging.info("start discharging")
+        #enable pins for relay
+            
+        
 sC = senseController()
 sense.update_realtime(sC.webSocketCallback)
